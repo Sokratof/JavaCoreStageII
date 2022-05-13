24c4a77b07011c3f306058af1805e3023d4befa0
Sokratof <sokratof@mail.ru>
2022-05-13 10:52:24 +0300

    first commit

---
+++ src/HW1_Object/Main.java
@@ -0,0 +1,12 @@
+package HW1_Object;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person[] person= new Person[4];
+        person[0] = new Person("John", 200, 3, 400);
+        person[1] = new Person("Nik",200, 3, 400);
+        person[2] = new Person("Tom",200, 3, 400);
+        person[3] = new Person("Kris",200, 3, 400);
+    }
+}
---
+++ src/HW1_Object/Person.java
@@ -0,0 +1,15 @@
+package HW1_Object;
+
+public class Person {
+    private String name;
+    private int run;
+    private int jump;
+    private int swim;
+
+    Person(String name, int run, int jump, int swim) {
+        this.name = name;
+        this.jump = jump;
+        this.run = run;
+        this.swim = swim;
+    }
+}

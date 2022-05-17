8e6b084255493505689f4332c47a3a974f42f8f2
Sokratof <sokratof@mail.ru>
2022-05-17 10:32:56 +0300

    Refactoring_5

--- src/HW1_Object/Main.java
+++ src/HW1_Object/Main.java
@@ -2,21 +2,23 @@ package HW1_Object;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
 
         String NameTeam = "Name Team is Dream Team";
         Person[] person= new Person[4];
-        person[0] = new Person("John", 200, 2, 500);
-        person[1] = new Person("Nik",200, 2, 600);
-        person[2] = new Person("Tom",300, 3, 700);
+        person[0] = new Person("John", 200, 3, 500);
+        person[1] = new Person("Nik",50, 2, 600);
+        person[2] = new Person("Tom",30, 3, 12);
         person[3] = new Person("Kris",400, 3, 1100);
-        for (HW1_Object.Person Person : person){
-            System.out.println(Person.toString(person));
-        }
-        Course course = new Course(100,1,200);
+
+
+
+        Course course = new Course(100,2,200);
         Team team = new Team(person);
+        System.out.println();
         System.out.println(NameTeam);
+        System.out.println();
         String result = course.doIt(team);
-        System.out.println(result);
     }
 }
